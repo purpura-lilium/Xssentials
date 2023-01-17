854e8f5adf86633ec848aa37d4e0394d519c4e37
purpura-lilium <119676493+purpura-lilium@users.noreply.github.com>
2023-01-17 10:44:42 +0800

    Update Xssentials.sh

--- Xssentials.sh
+++ Xssentials.sh
@@ -1,8 +1,8 @@
 # Written by purpura_lilium
 echo "Installing Xssentials"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /Users/patrick.young/.bash_profile
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/patrick.young/.bash_profile
+echo '# Set PATH, MANPATH, etc., for Homebrew.' >> ~/.bash_profile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.bash_profile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 brew install python3 neofetch discord firefox steam vlc docker visual-studio-code node git nano cowsay spotify the-unarchiver wget speedtest-cli iterm2 dotnet kotlin rust
 brew install --cask wine-stable webtorrent
